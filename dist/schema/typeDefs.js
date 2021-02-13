37f416e66698ff26c1fa2adabb5e8b49c1340e70
Nathaniel Richards <46858886+NateTheDev1@users.noreply.github.com>
2021-02-13 17:53:32 -0500

    Dev (#2)
    * Fixed debugger and playground
    
    * Added github workflows
    
    * Added husky, fixed scripts
    
    * Husky hooks added
    
    * Added post install script
    
    * Pre-commit
    
    * Husky rolled back
    
    * Upgrade package
    
    * Precommit hook
    
    * Testing precommit
    
    * Working on .github
    
    * Update to workflow
    
    * New workflow
    
    * Using checkout v2
    
    * Added install
    
    * Added install

--- dist/schema/typeDefs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.typeDefs = void 0;
-const fs_1 = __importDefault(require("fs"));
-exports.typeDefs = fs_1.default.readFileSync('src/schema/schema.graphql').toString();
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZURlZnMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvc2NoZW1hL3R5cGVEZWZzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLDRDQUFvQjtBQUVQLFFBQUEsUUFBUSxHQUFHLFlBQUUsQ0FBQyxZQUFZLENBQUMsMkJBQTJCLENBQUMsQ0FBQyxRQUFRLEVBQUUsQ0FBQyJ9

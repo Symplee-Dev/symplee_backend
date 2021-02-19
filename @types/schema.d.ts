112429d9c2fc6786df4e8e4d7b29ea21125e9c30
NateTheDev1 <nthnlrichards@gmail.com>
2021-02-19 10:29:48 -0500

    Resolved merge conflicts

--- @types/schema.d.ts
+++ @types/schema.d.ts
@@ -1,239 +1,147 @@
-declare global {
-	namespace Schema {
-		type Maybe<T> = T | undefined;
-		type Exact<T extends { [key: string]: unknown }> = {
-			[K in keyof T]: T[K];
-		};
-		type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-			{ [SubKey in K]?: Maybe<T[SubKey]> };
-		type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-			{ [SubKey in K]: Maybe<T[SubKey]> };
-		/** All built-in and custom scalars, mapped to their actual values */
-		interface Scalars {
-			ID: string;
-			String: string;
-			Boolean: boolean;
-			Int: number;
-			Float: number;
-		}
-
-		interface Query {
-			__typename?: 'Query';
-			test: Scalars['String'];
-			user: User;
-		}
-
-		interface QueryUserArgs {
-			id: Scalars['Int'];
-		}
-
-		interface Mutation {
-			__typename?: 'Mutation';
-			signup: User;
-			login?: Maybe<LoginReturn>;
-			verifyEmail: Scalars['Boolean'];
-		}
-
-		interface MutationSignupArgs {
-			user: UserInput;
-		}
-
-		interface MutationLoginArgs {
-			credentials: LoginInput;
-		}
-
-		interface MutationVerifyEmailArgs {
-			token: Scalars['String'];
-		}
-
-		interface LoginInput {
-			username?: Maybe<Scalars['String']>;
-			email?: Maybe<Scalars['String']>;
-			password: Scalars['String'];
-		}
-
-		interface LoginReturn {
-			__typename?: 'LoginReturn';
-			authenticated: Scalars['Boolean'];
-			token: Scalars['String'];
-		}
-
-		interface UserInput {
-			email: Scalars['String'];
-			name: Scalars['String'];
-			username: Scalars['String'];
-			password: Scalars['String'];
-		}
-
-		interface Schema {
-			__typename?: 'schema';
-			query?: Maybe<Query>;
-		}
-
-		interface User {
-			__typename?: 'User';
-			id: Scalars['Int'];
-			email: Scalars['String'];
-			name: Scalars['String'];
-			username: Scalars['String'];
-			key: Scalars['String'];
-			chatGroups: Array<ChatGroup>;
-			createdAt: Scalars['String'];
-			verified: Scalars['Boolean'];
-		}
-
-		interface ChatGroup {
-			__typename?: 'ChatGroup';
-			id: Scalars['Int'];
-			name: Scalars['String'];
-			isPublic: Scalars['Boolean'];
-			createdAt: Scalars['String'];
-			chats: Array<Maybe<Chat>>;
-		}
-
-		interface Chat {
-			__typename?: 'Chat';
-			id: Scalars['Int'];
-			name: Scalars['String'];
-			isPublic: Scalars['Boolean'];
-			createdById: Scalars['Int'];
-			messages: Array<Maybe<Message>>;
-		}
-
-		interface Message {
-			__typename?: 'Message';
-			id: Scalars['Int'];
-			body: Scalars['String'];
-			authorId: Scalars['Int'];
-			chatId: Scalars['Int'];
-			createdAt: Scalars['String'];
-		}
-	}
+declare global { namespace Schema {
+type Maybe<T> = T | undefined;
+type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+interface Scalars {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
 }
 
 interface Query {
-	__typename?: 'Query';
-	test: Scalars['String'];
-	user: User;
-	changeLogById: ChangeLog;
-	changeLogs: Array<Maybe<ChangeLog>>;
+  __typename?: 'Query';
+  test: Scalars['String'];
+  user: User;
+  changeLogById: ChangeLog;
+  changeLogs: Array<Maybe<ChangeLog>>;
 }
+
 
 interface QueryUserArgs {
-	id: Scalars['Int'];
+  id: Scalars['Int'];
 }
 
+
 interface QueryChangeLogByIdArgs {
-	id: Scalars['Int'];
+  id: Scalars['Int'];
 }
 
 interface Mutation {
-	__typename?: 'Mutation';
-	signup: User;
-	login?: Maybe<LoginReturn>;
-	verifyEmail: Scalars['Boolean'];
-	addNewChangeLog: ChangeLog;
-	editChangeLog?: Maybe<ChangeLog>;
+  __typename?: 'Mutation';
+  signup: User;
+  login?: Maybe<LoginReturn>;
+  verifyEmail: Scalars['Boolean'];
+  addNewChangeLog: ChangeLog;
+  editChangeLog?: Maybe<ChangeLog>;
 }
+
 
 interface MutationSignupArgs {
-	user: UserInput;
+  user: UserInput;
 }
+
 
 interface MutationLoginArgs {
-	credentials: LoginInput;
+  credentials: LoginInput;
 }
+
 
 interface MutationVerifyEmailArgs {
-	token: Scalars['String'];
+  token: Scalars['String'];
 }
+
 
 interface MutationAddNewChangeLogArgs {
-	newChangeLog: NewChangeLog;
+  newChangeLog: NewChangeLog;
 }
 
+
 interface MutationEditChangeLogArgs {
-	id: Scalars['Int'];
-	editChangeLog?: Maybe<NewChangeLog>;
+  id: Scalars['Int'];
+  editChangeLog?: Maybe<NewChangeLog>;
 }
 
 interface LoginInput {
-	username?: Maybe<Scalars['String']>;
-	email?: Maybe<Scalars['String']>;
-	password: Scalars['String'];
+  username?: Maybe<Scalars['String']>;
+  email?: Maybe<Scalars['String']>;
+  password: Scalars['String'];
 }
 
 interface ChangeLog {
-	__typename?: 'ChangeLog';
-	id: Scalars['Int'];
-	body: Scalars['String'];
-	changes: Array<Scalars['String']>;
-	created_at: Scalars['String'];
-	updated_at: Scalars['String'];
-	version: Scalars['String'];
+  __typename?: 'ChangeLog';
+  id: Scalars['Int'];
+  body: Scalars['String'];
+  changes: Array<Scalars['String']>;
+  created_at: Scalars['String'];
+  updated_at: Scalars['String'];
+  version: Scalars['String'];
 }
 
 interface LoginReturn {
-	__typename?: 'LoginReturn';
-	authenticated: Scalars['Boolean'];
-	token: Scalars['String'];
+  __typename?: 'LoginReturn';
+  authenticated: Scalars['Boolean'];
+  token: Scalars['String'];
 }
 
 interface NewChangeLog {
-	body: Scalars['String'];
-	changes: Array<Scalars['String']>;
-	version: Scalars['String'];
+  body: Scalars['String'];
+  changes: Array<Scalars['String']>;
+  version: Scalars['String'];
 }
 
 interface UserInput {
-	email: Scalars['String'];
-	name: Scalars['String'];
-	username: Scalars['String'];
-	password: Scalars['String'];
+  email: Scalars['String'];
+  name: Scalars['String'];
+  username: Scalars['String'];
+  password: Scalars['String'];
 }
 
 interface Schema {
-	__typename?: 'schema';
-	query?: Maybe<Query>;
+  __typename?: 'schema';
+  query?: Maybe<Query>;
 }
 
 interface User {
-	__typename?: 'User';
-	id: Scalars['Int'];
-	email: Scalars['String'];
-	name: Scalars['String'];
-	username: Scalars['String'];
-	key: Scalars['String'];
-	chatGroups: Array<ChatGroup>;
-	createdAt: Scalars['String'];
-	verified: Scalars['Boolean'];
+  __typename?: 'User';
+  id: Scalars['Int'];
+  email: Scalars['String'];
+  name: Scalars['String'];
+  username: Scalars['String'];
+  key: Scalars['String'];
+  chatGroups: Array<ChatGroup>;
+  createdAt: Scalars['String'];
+  verified: Scalars['Boolean'];
 }
 
 interface ChatGroup {
-	__typename?: 'ChatGroup';
-	id: Scalars['Int'];
-	name: Scalars['String'];
-	isPublic: Scalars['Boolean'];
-	createdAt: Scalars['String'];
-	chats: Array<Maybe<Chat>>;
+  __typename?: 'ChatGroup';
+  id: Scalars['Int'];
+  name: Scalars['String'];
+  isPublic: Scalars['Boolean'];
+  createdAt: Scalars['String'];
+  chats: Array<Maybe<Chat>>;
 }
 
 interface Chat {
-	__typename?: 'Chat';
-	id: Scalars['Int'];
-	name: Scalars['String'];
-	isPublic: Scalars['Boolean'];
-	createdById: Scalars['Int'];
-	messages: Array<Maybe<Message>>;
+  __typename?: 'Chat';
+  id: Scalars['Int'];
+  name: Scalars['String'];
+  isPublic: Scalars['Boolean'];
+  createdById: Scalars['Int'];
+  messages: Array<Maybe<Message>>;
 }
 
 interface Message {
-	__typename?: 'Message';
-	id: Scalars['Int'];
-	body: Scalars['String'];
-	authorId: Scalars['Int'];
-	chatId: Scalars['Int'];
-	createdAt: Scalars['String'];
+  __typename?: 'Message';
+  id: Scalars['Int'];
+  body: Scalars['String'];
+  authorId: Scalars['Int'];
+  chatId: Scalars['Int'];
+  createdAt: Scalars['String'];
 }
 
-export {};
+} } export {};

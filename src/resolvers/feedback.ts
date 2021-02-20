9faa26c07830e9a9f1572c8ab011fa89634198a2
Nathaniel Richards <nthnlrichards@gmail.com>
2021-02-20 17:01:45 -0500

    Feedback integrated

--- src/resolvers/feedback.ts
+++ src/resolvers/feedback.ts
@@ -1,4 +1,6 @@
+import { visitFunctionBody } from 'typescript';
 import AppFeedback from '../models/AppFeedback';
+import { EmailService } from '../Services/Email';
 import { Sanitizer } from '../utils/Cleaner';
 
 export const sendFeedback = async (
@@ -6,7 +8,7 @@ export const sendFeedback = async (
 	args: Resolvers.MutationSendFeedbackArgs,
 	context: Services.ServerContext
 ): Promise<Resolvers.AppFeedback> => {
-	const { userEmail, userName } = args.feedback;
+	const { userEmail, userName, body } = args.feedback;
 
 	context.logger.info(
 		'Trying to send feedback from: ' + userName ?? userEmail
@@ -14,8 +16,17 @@ export const sendFeedback = async (
 
 	const sanitizer = new Sanitizer(args.feedback);
 
-	const created = await AppFeedback.query().insertAndFetch(
-		sanitizer.checkAll()
+	const created = await AppFeedback.query().insertAndFetch({
+		...sanitizer.checkAll(),
+		createdAt: new Date().toString()
+	});
+
+	await new EmailService(context, '').sendPlainEmail(
+		userEmail,
+		'We received your feedback!',
+		'We will get back to you as soon as possible!',
+		'<h1>We will get back to you soom!</h1> <br> <p>If you have more questions reach out to us at legendarycomedy1@gmail.com</p> <br> <h1>Transcript</h1> ' +
+			body.toString()
 	);
 
 	if (!created) {

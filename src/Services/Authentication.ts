203e765b60bed1c92f5076fd583e1616becbb555
Nathaniel Richards <46858886+NateTheDev1@users.noreply.github.com>
2021-02-26 11:12:51 -0500

    Update Authentication.ts

--- src/Services/Authentication.ts
+++ src/Services/Authentication.ts
@@ -33,7 +33,7 @@ export const signJWT = async (
 
 	const signed = await jwt.sign(
 		{ ...session.session },
-		Config.SESSION_SECRET ?? 'supersecret',
+		process.env.NODE_ENV === "test" || prcoess.env.NODE_ENV === "TEST" ? 'supersecret' : Config.SESSION_SECRET,
 		JWTOptions
 	);
 
